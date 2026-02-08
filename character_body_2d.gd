15eeb664272e0b4e135a669db0664b1a572b77bb
breevee-admin <briannavigoa@gmail.com>
2026-02-08 13:36:13 -0800

    3

---
+++ character_body_2d.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+@export var speed = 1000
+
+var target = position
+
+func _input(event):
+	if event.is_action_pressed(&"click"):
+		target = get_global_mouse_position()
+	if event is InputEventMouseMotion and Input.is_action_pressed(&"click"):
+		target = get_global_mouse_position()
+	look_at(get_global_mouse_position())
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	if position.distance_to(target) > 10:
+		move_and_slide()
